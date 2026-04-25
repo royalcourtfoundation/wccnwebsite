16074aa33cea413ca83aabe92e6866bb73f3ab27
royalcourtfoundation <royalcourtfoundation@gmail.com>
2026-04-25 16:44:27 +0530

    Create LayoutIndex.js

---
+++ src/Pages/Layout/LayoutIndex.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../Component/Header'
+import Footer from '../../Component/Footer'
+import Index from '../Index'
+
+function LayoutIndex() {
+  return (
+    <>
+      <Header/>
+      <Index/>
+      <Footer/>
+    </>
+  )
+}
+
+export default LayoutIndex
