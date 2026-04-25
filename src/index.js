62781e52e6000b5bd3827176f8aae19276aaedea
royalcourtfoundation <royalcourtfoundation@gmail.com>
2026-04-25 17:17:13 +0530

    Simplify root rendering in index.js
    Refactored root rendering code for ReactDOM.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ import '../src/Assets/CSS/Aboutus.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
