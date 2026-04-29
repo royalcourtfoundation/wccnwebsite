88b6870e337a9feb038495aea3b9c30dee41d7f1
royalcourtfoundation <royalcourtfoundation@gmail.com>
2026-04-29 19:13:28 +0530

    Create LayoutTreasurers component with header and footer

---
+++ src/Pages/Layout/LayoutTreasurers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../Component/Header'
+import Treasurers from '../Treasurers'
+import Footer from '../../Component/Footer'
+
+function LayoutTreasurers() {
+    return (
+        <>
+            <Header />
+            <Treasurers />
+            <Footer />
+        </>
+    )
+}
+
+export default LayoutTreasurers
