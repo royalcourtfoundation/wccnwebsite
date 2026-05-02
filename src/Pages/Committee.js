4b3ad9df279c25d4ab6134dd1a2d10c26ba43fbf
royalcourtfoundation <royalcourtfoundation@gmail.com>
2026-05-02 10:57:07 +0530

    Update Committee.js

--- src/Pages/Committee.js
+++ src/Pages/Committee.js
@@ -42,8 +42,7 @@ export default function Committee() {
 
                             "Dr. Jan Fedacko, MD, PhD, FICN, Slovakia"
                         ].map((item, i) => (
-                            <div key={i} className="committee-card">
-                             /* <div className="committee-icon">👤</div> */
+                            <div key={i} className="committee-card">                           
                             <p>{item}</p>
                             </div>
                         ))}
